f035a2326530c6d6fbfac6257dae860d583a53e7
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2023-11-14 14:35:17 -0500

    start wall jump code

--- scenes/player.gd
+++ scenes/player.gd
@@ -34,8 +34,12 @@ func _ready():
 
 func _physics_process(delta):
 	
+	var direction = Input.get_axis("left", "right")
+	
 	match  currentMoveState:
 		MOVE_STATES.WALK:
+			$AnimatedSprite2D.play("run")
+			
 			# Add the gravity.
 			if not is_on_floor():
 				velocity.y += gravity * delta
@@ -58,19 +62,47 @@ func _physics_process(delta):
 			
 			# Get the input direction and handle the movement/deceleration.
 			# As good practice, you should replace UI actions with custom gameplay actions.
-			var direction = Input.get_axis("left", "right")
+
 			
 			var currentAccel = (GROUND_ACCEL if is_on_floor() else AIR_ACCEL) * delta
 			var currentStopAccel = (GROUND_STOP_ACCEL if is_on_floor() else AIR_STOP_ACCEL) * delta
-			
 			if direction:
-				velocity.x = lerp(velocity.x,-direction * SPEED,currentAccel)
+				velocity.x = lerp(velocity.x,direction * SPEED,currentAccel)
 			else:
 				velocity.x = lerp(velocity.x,0.0,currentStopAccel)
 			
-			
+			if is_on_wall_only():
+				if get_last_slide_collision().get_normal() == Vector2.RIGHT and velocity.x > 0:
+					currentMoveState = MOVE_STATES.LEFTWALLCLIMB
+				if get_last_slide_collision().get_normal() == Vector2.LEFT and velocity.x < 0:
+					currentMoveState = MOVE_STATES.LEFTWALLCLIMB
+				
 			
 		MOVE_STATES.LEFTWALLCLIMB or MOVE_STATES.RIGHTWALLCLIMB:
+			
+			$AnimatedSprite2D.play("wall slide")
+			
+			if (not is_on_wall_only()) or is_on_ceiling():
+				currentMoveState = MOVE_STATES.WALK
+			
+			var currentAccel = GROUND_ACCEL * delta
+			if direction:
+				velocity.x = lerp(velocity.x,direction * SPEED,currentAccel)
+			
+			print(velocity)
+			
+			remainingJumps = 2
+			
+			if Input.is_action_just_pressed("up") and canJump:
+				velocity.y = JUMP_FORCE
+				
+				
+				velocity.x += 500 if currentMoveState == MOVE_STATES.LEFTWALLCLIMB else -500
+				
+				remainingJumps -= 1
+				if remainingJumps <= 0:
+					canJump = false
+			
 			pass
 	
 	
@@ -78,6 +110,11 @@ func _physics_process(delta):
 	move_and_slide()
 	
 
+func setFlipH(flipH):
+	$AnimatedSprite2D.flip_h = flipH
+	
+	
+	
 
 func _on_coyote_time_timeout():
 	
