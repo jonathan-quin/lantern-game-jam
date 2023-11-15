fd1d9404a73f46b87f19b66e466d0bddbfe9b9c6
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2023-11-15 11:41:31 -0500

    added stuff

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,8 +2,18 @@ extends CharacterBody2D
 
 enum MOVE_STATES {WALK,LEFTWALLCLIMB,RIGHTWALLCLIMB,DASH}
 
+const WALLCLIMB_FALLSPEED = 20
+const WALLJUMP_HORIZONTAL_FORCE = 300
+const WALLCLIMB_ACCEL = 500
 
-var currentMoveState = MOVE_STATES.WALK
+
+
+var currentMoveStateField = MOVE_STATES.WALK
+var currentMoveState: MOVE_STATES:
+	get:
+		return currentMoveStateField
+	set(value):
+		currentMoveStateField = value
 
 const SPEED = 300.0
 const BRIGHT_SPEED = 600.0
@@ -36,75 +46,80 @@ func _physics_process(delta):
 	
 	var direction = Input.get_axis("left", "right")
 	
-	match  currentMoveState:
-		MOVE_STATES.WALK:
-			$AnimatedSprite2D.play("run")
+	if currentMoveState == MOVE_STATES.WALK:
+		$AnimatedSprite2D.play("run")
+		
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			if !$coyoteTime.is_stopped():
+				$coyoteTime.start()
+		else:
+			$coyoteTime.wait_time = COYOTE_TIME
+			canJump = true
+			remainingJumps = MAX_JUMPS
+		
+		
+		
+		# Handle Jump.
+		if Input.is_action_just_pressed("up") and canJump:
+			velocity.y = JUMP_FORCE
 			
-			# Add the gravity.
-			if not is_on_floor():
-				velocity.y += gravity * delta
-				if !$coyoteTime.is_stopped():
-					$coyoteTime.start()
-			else:
-				$coyoteTime.wait_time = COYOTE_TIME
-				canJump = true
-				remainingJumps = MAX_JUMPS
+			remainingJumps -= 1
+			if remainingJumps <= 0:
+				canJump = false
+		
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		
+		var currentAccel = (GROUND_ACCEL if is_on_floor() else AIR_ACCEL) * delta
+		var currentStopAccel = (GROUND_STOP_ACCEL if is_on_floor() else AIR_STOP_ACCEL) * delta
+		if direction:
+			velocity.x = lerp(velocity.x,direction * SPEED,currentAccel)
+			setFlipH(direction < 0)
+		else:
+			velocity.x = lerp(velocity.x,0.0,currentStopAccel)
+		
+		if is_on_wall_only():
+			if get_last_slide_collision().get_normal() == Vector2.RIGHT and velocity.x < 0:
+				currentMoveState = MOVE_STATES.RIGHTWALLCLIMB
+			if get_last_slide_collision().get_normal() == Vector2.LEFT and velocity.x > 0:
+				currentMoveState = MOVE_STATES.LEFTWALLCLIMB
+			
+		
+	if currentMoveState == MOVE_STATES.LEFTWALLCLIMB or currentMoveState == MOVE_STATES.RIGHTWALLCLIMB:
+		
+		setFlipH(true if currentMoveState == MOVE_STATES.LEFTWALLCLIMB else false)
+		
+		$AnimatedSprite2D.play("wall slide")
+		
+		if (not is_on_wall_only()) or is_on_ceiling():
+			currentMoveState = MOVE_STATES.WALK
+		
+		velocity.y += gravity * delta
+		if velocity.y > WALLCLIMB_FALLSPEED:
+			velocity.y = WALLCLIMB_FALLSPEED
+		
+		var currentAccel = GROUND_ACCEL * delta
+		if direction:
+			velocity.x = lerp(velocity.x,direction * SPEED,currentAccel)
+		
+		
+		
+		remainingJumps = min(remainingJumps,2)
+		canJump = true
+		
+		if Input.is_action_just_pressed("up") and canJump:
+			velocity.y = JUMP_FORCE
 			
 			
+			velocity.x = -WALLJUMP_HORIZONTAL_FORCE if currentMoveState == MOVE_STATES.LEFTWALLCLIMB else WALLJUMP_HORIZONTAL_FORCE
 			
-			# Handle Jump.
-			if Input.is_action_just_pressed("up") and canJump:
-				velocity.y = JUMP_FORCE
-				
-				remainingJumps -= 1
-				if remainingJumps <= 0:
-					canJump = false
-			
-			# Get the input direction and handle the movement/deceleration.
-			# As good practice, you should replace UI actions with custom gameplay actions.
-
-			
-			var currentAccel = (GROUND_ACCEL if is_on_floor() else AIR_ACCEL) * delta
-			var currentStopAccel = (GROUND_STOP_ACCEL if is_on_floor() else AIR_STOP_ACCEL) * delta
-			if direction:
-				velocity.x = lerp(velocity.x,direction * SPEED,currentAccel)
-			else:
-				velocity.x = lerp(velocity.x,0.0,currentStopAccel)
-			
-			if is_on_wall_only():
-				if get_last_slide_collision().get_normal() == Vector2.RIGHT and velocity.x > 0:
-					currentMoveState = MOVE_STATES.LEFTWALLCLIMB
-				if get_last_slide_collision().get_normal() == Vector2.LEFT and velocity.x < 0:
-					currentMoveState = MOVE_STATES.LEFTWALLCLIMB
-				
-			
-		MOVE_STATES.LEFTWALLCLIMB or MOVE_STATES.RIGHTWALLCLIMB:
-			
-			$AnimatedSprite2D.play("wall slide")
-			
-			if (not is_on_wall_only()) or is_on_ceiling():
-				currentMoveState = MOVE_STATES.WALK
-			
-			var currentAccel = GROUND_ACCEL * delta
-			if direction:
-				velocity.x = lerp(velocity.x,direction * SPEED,currentAccel)
-			
-			print(velocity)
-			
-			remainingJumps = 2
-			
-			if Input.is_action_just_pressed("up") and canJump:
-				velocity.y = JUMP_FORCE
-				
-				
-				velocity.x += 500 if currentMoveState == MOVE_STATES.LEFTWALLCLIMB else -500
-				
-				remainingJumps -= 1
-				if remainingJumps <= 0:
-					canJump = false
-			
-			pass
-	
+			remainingJumps -= 1
+			if remainingJumps <= 0:
+				canJump = false
+		
+		pass
 	
 	
 	move_and_slide()
@@ -113,7 +128,11 @@ func _physics_process(delta):
 func setFlipH(flipH):
 	$AnimatedSprite2D.flip_h = flipH
 	
-	
+	var value = 1 if flipH else -1
+	for child in $invertableStuff.get_children():
+		child.position.x = abs(child.position.x )* value
+		print(flipH)
+		print(child)
 	
 
 func _on_coyote_time_timeout():
