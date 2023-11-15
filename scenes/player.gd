58e7404cf6ae9b56f97615a5d2e6c586302a795c
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2023-11-15 14:12:54 -0500

    added proper wall jumps

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 enum MOVE_STATES {WALK,LEFTWALLCLIMB,RIGHTWALLCLIMB,DASH}
 
-const WALLCLIMB_FALLSPEED = 20
+const WALLCLIMB_FALLSPEED = 30
+const WALLJUMP_JUMP_FORCE = -350
 const WALLJUMP_HORIZONTAL_FORCE = 300
-const WALLCLIMB_ACCEL = 500
+const WALLCLIMB_GRAVITY = 350
 
 
 
@@ -15,8 +16,8 @@ var currentMoveState: MOVE_STATES:
 	set(value):
 		currentMoveStateField = value
 
-const SPEED = 300.0
-const BRIGHT_SPEED = 600.0
+const SPEED = 200.0
+const BRIGHT_SPEED = 300.0
 
 const GROUND_ACCEL = 6
 const AIR_ACCEL = 3
@@ -36,6 +37,21 @@ const MAX_JUMPS = 2
 var gravity = 1000
 const COYOTE_TIME = 0.1
 
+
+var lanternLitField = false
+var lanternLit: 
+	get:
+		return lanternLitField
+	set(value):
+		if lanternLit != value:
+			if !value:
+				$lightAnimationPlayer.play("light_idle")
+			else:
+				$lightAnimationPlayer.play("light_flare")
+			
+		
+		lanternLitField = value
+
 func _ready():
 	
 	$lightAnimationPlayer.play("light_idle")
@@ -45,9 +61,12 @@ func _ready():
 func _physics_process(delta):
 	
 	var direction = Input.get_axis("left", "right")
+	#Dani should lock their screen when they leave their laptop.
+	
+	lanternLit = Input.is_action_pressed("lightLantern")
 	
 	if currentMoveState == MOVE_STATES.WALK:
-		$AnimatedSprite2D.play("run")
+		
 		
 		# Add the gravity.
 		if not is_on_floor():
@@ -63,7 +82,7 @@ func _physics_process(delta):
 		
 		# Handle Jump.
 		if Input.is_action_just_pressed("up") and canJump:
-			velocity.y = JUMP_FORCE
+			velocity.y = BRIGHT_JUMP_FORCE if lanternLit else JUMP_FORCE
 			
 			remainingJumps -= 1
 			if remainingJumps <= 0:
@@ -74,26 +93,31 @@ func _physics_process(delta):
 		
 		var currentAccel = (GROUND_ACCEL if is_on_floor() else AIR_ACCEL) * delta
 		var currentStopAccel = (GROUND_STOP_ACCEL if is_on_floor() else AIR_STOP_ACCEL) * delta
+		
+		var currentSpeed = BRIGHT_SPEED if lanternLit else SPEED
+		
 		if direction:
-			velocity.x = lerp(velocity.x,direction * SPEED,currentAccel)
+			velocity.x = lerp(velocity.x,direction * currentSpeed,currentAccel)
 			setFlipH(direction < 0)
+			$AnimatedSprite2D.play("run")
 		else:
 			velocity.x = lerp(velocity.x,0.0,currentStopAccel)
+			$AnimatedSprite2D.play("idle")
 		
-		if is_on_wall_only():
+		if is_on_wall_only() and $wallChecks/touchingWall.is_colliding() and !$wallChecks/touchingFloor.is_colliding():
 			if get_last_slide_collision().get_normal() == Vector2.RIGHT and velocity.x < 0:
 				currentMoveState = MOVE_STATES.RIGHTWALLCLIMB
 			if get_last_slide_collision().get_normal() == Vector2.LEFT and velocity.x > 0:
 				currentMoveState = MOVE_STATES.LEFTWALLCLIMB
 			
 		
-	if currentMoveState == MOVE_STATES.LEFTWALLCLIMB or currentMoveState == MOVE_STATES.RIGHTWALLCLIMB:
+	elif currentMoveState == MOVE_STATES.LEFTWALLCLIMB or currentMoveState == MOVE_STATES.RIGHTWALLCLIMB:
 		
 		setFlipH(true if currentMoveState == MOVE_STATES.LEFTWALLCLIMB else false)
 		
 		$AnimatedSprite2D.play("wall slide")
 		
-		if (not is_on_wall_only()) or is_on_ceiling():
+		if !$wallChecks/touchingWall.is_colliding() or $wallChecks/touchingFloor.is_colliding():
 			currentMoveState = MOVE_STATES.WALK
 		
 		velocity.y += gravity * delta
@@ -106,11 +130,11 @@ func _physics_process(delta):
 		
 		
 		
-		remainingJumps = min(remainingJumps,2)
+		remainingJumps = 2
 		canJump = true
 		
 		if Input.is_action_just_pressed("up") and canJump:
-			velocity.y = JUMP_FORCE
+			velocity.y = WALLJUMP_JUMP_FORCE
 			
 			
 			velocity.x = -WALLJUMP_HORIZONTAL_FORCE if currentMoveState == MOVE_STATES.LEFTWALLCLIMB else WALLJUMP_HORIZONTAL_FORCE
@@ -128,7 +152,7 @@ func _physics_process(delta):
 func setFlipH(flipH):
 	$AnimatedSprite2D.flip_h = flipH
 	
-	var value = 1 if flipH else -1
+	var value = -1 if flipH else 1
 	for child in $invertableStuff.get_children():
 		child.position.x = abs(child.position.x )* value
 		print(flipH)
